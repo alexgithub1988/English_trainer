21ca0c5d856156cbb6ab66fc54904fe781158a86
alexgithub1988 <a.s.egorenkov@gmail.com>
2026-05-03 11:08:49 +0400

    some changes

--- engine.py
+++ engine.py
@@ -1,5 +1,36 @@
+from dataclasses import dataclass, field
+
 @dataclass
-class Word:
-    english: str
+class Sentence:
     russian: str
+    english: str
+    words: list = field(default_factory=list)
+    fake_pronouns: list = field(default_factory=list)
+    fake_verbs: list = field(default_factory=list)
+
+
+
+
+    def check(self, answer: str) -> bool:
+        """
+        Сверяем ответ
+        :param answer:
+        :return:
+        """
+        if self.english == answer:
+            return True
+        else:
+            return False
+
+    def get_words(self) -> list:
+        return self.words
+
+@dataclass
+class Lesson:
+    sentence: Sentence = field(default_factory=Sentence)
+    fake_pronouns: list = field(default_factory=list)
+    fake_verbs: list = field(default_factory=list)
+
+
+
 
